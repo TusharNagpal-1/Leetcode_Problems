ec49d8fd3a8163a3c7c2f84f2feca8916459a606
Tushar Nagpal <nagpaltushar669@gmail.com>
2026-07-06 18:31:50 +0530

    Time: 1414 ms (5.85%) | Memory: 225.4 MB (5.42%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-int levels(TreeNode* root){
-    if(root==NULL) return 0;
-    return 1+max(levels(root->left),levels(root->right));
-}
-void nthlevel(TreeNode* root,int curr,int level,vector<vector<int>> &v){
-    if(root==NULL) return;
-    if(curr==level) {
-        v[level-1].push_back(root->val);
-        return;
+    int levels(TreeNode* root){
+        if(root==nullptr) return 0;
+        return 1+max(levels(root->left),levels(root->right));
     }
-    nthlevel(root->left,curr+1,level,v);
-    nthlevel(root->right,curr+1,level,v);
-}
-void levelorder(TreeNode* root,vector<vector<int>>& v){
-    int n=levels(root);
-    v.resize(n);
-    for(int i=1;i<=n;i++){
-        nthlevel(root,1,i,v);
+    vector<int> levelOrder(TreeNode* root,int cur,int level,vector<int>& v){
+        if(root==NULL) return {};
+        if(cur==level) v.push_back(root->val);
+        levelOrder(root->left,cur+1,level,v);
+        levelOrder(root->right,cur+1,level,v);
+        return v;
+        // v.erase();
     }
-}
     vector<vector<int>> levelOrder(TreeNode* root) {
+        int n=levels(root);
         vector<vector<int>> v;
-        levelorder(root,v);
+        for(int i=1;i<=n;i++){
+            vector<int> v1;
+            v.push_back(levelOrder(root,1,i,v1));
+        }
         return v;
     }
 };
