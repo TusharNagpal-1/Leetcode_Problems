634718663cdc325f1d6e2c30fc8f4b26500886cf
Tushar Nagpal <nagpaltushar669@gmail.com>
2026-07-24 08:06:36 +0530

    Time: 3 ms (31.61%) | Memory: 10.5 MB (51.19%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -7,11 +7,11 @@ public:
         }
         ans.push_back(nums[i]);
         allsubset(v,ans,nums,i+1);
+        ans.pop_back();
         int idx=i+1;
         while(idx<nums.size() && nums[idx]==nums[idx-1]){
             idx++;
         }
-        ans.pop_back();
         allsubset(v,ans,nums,idx);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
