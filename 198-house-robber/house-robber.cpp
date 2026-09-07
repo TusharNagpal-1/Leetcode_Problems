07977ede586fa1652a4596fbe6608af34b0dccc0
Tushar Nagpal <nagpaltushar669@gmail.com>
2026-09-07 14:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (6.04%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int helper(vector<int>& v,int n,vector<vector<int>>& dp,int i,int free){
-        if(i==n) return 0;
-        if(dp[i][free]!=-1) return dp[i][free];
-        if(free == 0){
-            return dp[i][free]=helper(v,n,dp,i+1,1);
-        }
-        int c1=v[i]+helper(v,n,dp,i+1,0);
-        int c2=helper(v,n,dp,i+1,1);
-        return dp[i][free]=max(c1,c2);
+      if(i==n){
+        return 0;
+      }
+      if(dp[i][free]!=-1) return dp[i][free];
+      if(free==1){
+        return dp[i][free]=helper(v,n,dp,i+1,0);
+      }
+      int c1=v[i]+helper(v,n,dp,i+1,1);
+      int c2=helper(v,n,dp,i+1,0);
+      return dp[i][free]=max(c1,c2);
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n);
-        for(int i=0;i<n;i++){
-            vector<int> t(2,-1);
-            dp[i]=t;
-        }
-        return helper(nums,n,dp,0,1);
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
+       return helper(nums,n,dp,0,0);
     }
 };
