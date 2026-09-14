f6bca45b8d59ebc68cc86bf8bc5ad09bd2031ff9
Tushar Nagpal <nagpaltushar669@gmail.com>
2026-09-14 16:13:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (98.22%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,19 +3,19 @@ public:
     bool isValid(string s) {
         int n=s.size();
         stack<char> st;
-        for(int i=0;i<n;i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
+        for(char c:s){
+            if(c=='[' || c=='{' || c=='('){
+                st.push(c);
             }
-            else if((s[i]==')'  || s[i]=='}'  || s[i]==']') && (st.size()!=0)){
-                if(st.top()=='(' && s[i]==')') st.pop();
-                else if(st.top()=='{' && s[i]=='}') st.pop();
-                else if(st.top()=='[' && s[i]==']') st.pop();
-                else return false;
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                char x=st.top();
+                st.pop();
+                if((c==')' && x!='(') || (c=='}' && x!='{') || (c==']' && x!='[')) return false;   
             }
-            else return false;
         }
-        if(st.size()==0) return true;
-        else return false; 
+        return st.empty();
     }
 };
